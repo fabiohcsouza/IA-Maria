1f8a9901a4061f2a26acb82e7ed5eec76fec194e
Fabio Souza <80291827+fabiohcsouza@users.noreply.github.com>
2021-10-01 23:49:58 -0300

    Add modelo vosk

---
+++ main.py
@@ -0,0 +1,11 @@
+# Our main file.
+import speech_recognition as sr
+
+# Cria um reconhecedor
+r = sr.Recognizer()
+
+# Abrir o microfone para captura
+with sr.Microphone() as source:
+    while True:
+        audio = r.listen(source)  # Define microfone como fonte de audio
+        print(r.recognize_google(audio, language='pt'))
